08e37a8557b9e46ac7afb4ef3b7dfdbc18603931
linjiansi <pmon.ken.1467@gmail.com>
2023-04-03 02:51:46 +0900

    added cli function which is converting to gif from video file

--- cmd/root.go
+++ cmd/root.go
@@ -22,12 +22,16 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
-
+var inputFile string
+var outputFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -41,7 +45,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		convertVideoToGif(inputFile, outputFile)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -62,7 +68,82 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&inputFile, "input", "i", "", "Input video file")
+	rootCmd.PersistentFlags().StringVarP(&outputFile, "output", "o", "", "Output GIF file")
 }
 
+func convertVideoToGif(input string, output string) {
+	if input == "" || output == "" {
+		fmt.Println("Please specify input and output file paths")
+		return
+	}
 
+	ffmegCmd := exec.Command(
+		"ffmpeg",
+		"-i", input, output,
+	)
+
+	if checkFFmpegInstalled() {
+		installFFmpeg()
+	}
+
+	if err := deleteFile(".", output); err != nil {
+		fmt.Println(err)
+	}
+
+	err := ffmegCmd.Run()
+	if err != nil {
+		fmt.Printf("Error occured while converting video to GIF: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("GIF created successfully!")
+}
+
+func checkFFmpegInstalled() bool {
+	_, err := exec.LookPath("ffmpeg")
+	return err != nil
+}
+
+func installFFmpeg() {
+	fmt.Println("Attempting to install FFmpeg...")
+	switch runtime.GOOS {
+	case "darwin":
+		cmd := exec.Command("brew", "install", "ffmpeg")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to install FFmpeg using Homebrew: %v\n", err)
+			fmt.Println("Please install FFmpeg manually and try again.")
+			os.Exit(1)
+		} else {
+			fmt.Println("FFmpeg installed successfully!")
+		}
+	case "linux":
+		cmd := exec.Command("sudo", "apt", "install", "ffmpeg")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to install FFmpeg using apt: %v\n", err)
+			fmt.Println("Please install FFmpeg manually and try again.")
+			os.Exit(1)
+		} else {
+			fmt.Println("FFmpeg installed successfully!")
+		}
+	default:
+		fmt.Println("Unsupported platform. Please install FFmpeg manually and try again.")
+		os.Exit(1)
+	}
+}
+
+func deleteFile(directory string, filename string) error {
+	filePath := directory + string(os.PathSeparator) + filename
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return fmt.Errorf("File %s does not exist\n", filePath)
+	}
+
+	err := os.Remove(filePath)
+	if err != nil {
+		return fmt.Errorf("Failed to delete file %s: %v\n", filePath, err)
+	}
+
+	return nil
+}
